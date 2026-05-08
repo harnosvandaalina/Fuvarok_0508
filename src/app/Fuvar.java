84b6ff807a6dc19dc8e9f0dfa8df63c59bc360a6
harnosvandaalina <harnosvandaalina@gmail.com>
2026-05-08 14:20:03 +0200

    beolvasas kesz, feladatok megoldasra varnak

--- src/app/Fuvar.java
+++ src/app/Fuvar.java
@@ -2,9 +2,34 @@
 package app;
 
 public class Fuvar {
-    private enum Fizetes;
+    private Fizetes fizet;
     private String rsz;
     private int ido;
     private double osszeg;
+
+    public Fuvar(Fizetes fizet, String rsz, int ido, double osszeg) {
+        this.fizet = fizet;
+        this.rsz = rsz;
+        this.ido = ido;
+        this.osszeg = osszeg;
+    }
+
+    public Fizetes getFizet() {
+        return fizet;
+    }
+
+    public String getRsz() {
+        return rsz;
+    }
+
+    public int getIdo() {
+        return ido;
+    }
+
+    public double getOsszeg() {
+        return osszeg;
+    }
+ 
+    
     
 }
--- src/app/Kocsik.java
+++ src/app/Kocsik.java
@@ -1,11 +1,22 @@
 
 package app;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class Kocsik {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+       Path path = Path.of("fuvarok.csv");
        
+       List<String> sorok = Files.readAllLines(path);
+       
+        for (String sor : sorok) {
+            System.out.println("sor: " + sor);
+        }
     }
     
 }
